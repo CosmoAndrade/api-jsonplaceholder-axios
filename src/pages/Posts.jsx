b24ba1e96060280f85099d5f65e3de943b46c6f4
CosmoAndrade <cosmo.andrade@hotmail.com>
2022-08-14 21:38:51 -0300

    Parte 03

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -4,6 +4,8 @@ import './Posts.css'
 
 const Posts = () => {
     const [posts, setPosts] = useState([])
+    const [title, setTitle] = useState('');
+   const [body, setBody] = useState('');
 
     const getPosts = () => {
         api.get("posts")
@@ -17,12 +19,14 @@ const Posts = () => {
     useEffect(() => {
         getPosts()
     }, [])
+    
 
 
 
     return (
         <div>
             <h1>Posts</h1>
+           
 
             {posts.map((post,index) => {
                 return (
@@ -30,6 +34,7 @@ const Posts = () => {
                         <strong>{post.id}</strong>
                         <h3>{post.title}</h3>
                         <p>{post.body}</p>
+                        
                     </div>
                 )
             })}
