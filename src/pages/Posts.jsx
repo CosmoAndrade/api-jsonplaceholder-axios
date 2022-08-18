51b144e9b9f48de6ca88586a0268da35602abd81
CosmoAndrade <cosmo.andrade@hotmail.com>
2022-08-17 22:04:48 -0300

    Parte 04

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -5,7 +5,7 @@ import './Posts.css'
 const Posts = () => {
     const [posts, setPosts] = useState([])
     const [title, setTitle] = useState('');
-   const [body, setBody] = useState('');
+    const [body, setBody] = useState('');
 
     const getPosts = () => {
         api.get("posts")
@@ -18,23 +18,47 @@ const Posts = () => {
 
     useEffect(() => {
         getPosts()
+
+       
+
     }, [])
-    
+
+    const handleCreate = () => {
+        const request = {
+            title: 'foo',
+            body: 'bar',
+            userId: 1,
+        }
+
+        api.post("posts" , request)
+        .then((response) =>  getPosts())
+            .catch((err) => {
+                console.error("ops! ocorreu um erro" + err);
+            });
+    }
+
+
+
+
+
 
 
 
     return (
         <div>
             <h1>Posts</h1>
-           
+            <button onClick={handleCreate}>Add</button>
 
-            {posts.map((post,index) => {
+
+
+            {posts.map((post, index) => {
                 return (
                     <div key={index} className='posts'>
                         <strong>{post.id}</strong>
                         <h3>{post.title}</h3>
                         <p>{post.body}</p>
                         
+                       
                     </div>
                 )
             })}
